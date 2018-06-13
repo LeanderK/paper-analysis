f9bcddf143767d74a435b6d667349d86a6e3a72f
LeanderK <Leander.Kurscheidt@gmx.de>
2018-06-13 21:24:18 +0200

    Init

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.0.1',
+    description='analysis of the machine learning research output in germany',
+    author='Leander Kurscheidt, Tobi Böck',
+    license='MIT',
+)
